898227037cc803aa7b9b7fd4cc2fde36d5c0257a
OndrejVojik <vojik.cz@gmail.com>
2024-08-30 14:01:34 +0200

    11

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/vanlife/',
-  server: {
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        if (req.url.startsWith('/vanlife') && !req.url.includes('.')) {
-          req.url = '/index.html';
-        }
-        next();
-      });
-    },
-  },
 })
