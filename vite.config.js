a112fd64c8d35bc02f320969f362e0847ea47105
OndrejVojik <vojik.cz@gmail.com>
2024-08-30 11:59:22 +0200

    9

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/vanlife/',
+  server: {
+    historyApiFallback: true, // Enable HTML5 history API fallback
+  },
 })
