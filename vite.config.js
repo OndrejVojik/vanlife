41b786a12124bfe2b3ff4285b360eb96c8fdcf8a
OndrejVojik <vojik.cz@gmail.com>
2024-08-31 13:52:36 +0200

    27

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command, mode }) => {
   // Load environment variables based on the current mode (development or production)
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd(), '');
 
   // Use the loaded environment variable to set the base path
   const basePath = env.VITE_BASE_PATH || '/';
