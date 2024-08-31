95b58116ea82bb7c03f2f20d67c7111787d1802f
OndrejVojik <vojik.cz@gmail.com>
2024-08-31 16:50:07 +0200

    49

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command, mode }) => {
 
   return {
     plugins: [react()],
-    base: env.production.VITE_BASE_PATH,
+    base: env.VITE_BASE_PATH,
   };
 });
 // export default defineConfig(({ command, mode }) => {
