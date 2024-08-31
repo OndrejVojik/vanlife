fbbfe04d8ee24460c85b5d6bcf0d3e1745c262d3
OndrejVojik <vojik.cz@gmail.com>
2024-08-31 16:31:04 +0200

    46

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+export default defineConfig(({ command, mode }) => {
+  // Load environment variables based on the current mode (development or production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // Use the loaded environment variable to set the base path
+  const basePath = env.VITE_BASE_PATH || '/';
+
+  return {
+    plugins: [react()],
+    base: '/vanlife',
+  };
+});
 // export default defineConfig(({ command, mode }) => {
-//   // Load environment variables based on the current mode (development or production)
-//   const env = loadEnv(mode, process.cwd(), '');
-
-//   // Use the loaded environment variable to set the base path
-//   const basePath = env.VITE_BASE_PATH || '/';
-
+//   // Load env file based on `mode` in the current working directory.
+//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+//   const env = loadEnv(mode, process.cwd(), '')
 //   return {
-//     plugins: [react()],
-//     base: basePath,
-//   };
-// });
-export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-
-  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
-  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
-
-  return defineConfig({
-      plugins: [react()],
-      base: '/vanlife',
-  });
-}
+//     // vite config
+//     define: {
+//       __APP_ENV__: JSON.stringify(env.APP_BASE_PATH),
+//     },
+//   }
+// })
