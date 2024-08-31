0c9f195b11a272f0b5d67dba2ec3e0594acd1599
OndrejVojik <vojik.cz@gmail.com>
2024-08-31 19:36:10 +0200

    52

--- vite.config.js
+++ vite.config.js
@@ -13,14 +13,3 @@ export default defineConfig(({ command, mode }) => {
     base: basePath,
   };
 });
-// export default defineConfig(({ command, mode }) => {
-//   // Load env file based on `mode` in the current working directory.
-//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-//   const env = loadEnv(mode, process.cwd(), '')
-//   return {
-//     // vite config
-//     define: {
-//       __APP_ENV__: JSON.stringify(env.APP_BASE_PATH),
-//     },
-//   }
-// })
